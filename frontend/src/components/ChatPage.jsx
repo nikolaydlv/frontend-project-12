a67833280f09862f3b90ea4f69a47141132514cc
Nikolay <wokewoke@mail.ru>
2023-11-21 16:59:29 +0300

    add chat, login and 404 pages

---
+++ frontend/src/components/ChatPage.jsx
@@ -0,0 +1,5 @@
+const ChatPage = () => {
+
+};
+
+export default ChatPage;
---
+++ frontend/src/components/LoginPage.jsx
@@ -0,0 +1,105 @@
+/* eslint-disable functional/no-expression-statements */
+import * as yup from 'yup';
+import React, { useEffect, useRef, useState } from 'react';
+import { useFormik } from 'formik';
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  Button,
+  Form,
+} from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
+import loginImg from '../assets/login.jpg';
+
+const loginSchema = yup.object().shape({
+  name: yup.string().required(),
+  password: yup.string().required(),
+});
+
+const LoginPage = () => {
+  const inputNameRef = useRef();
+  const [formValid, setFormValid] = useState(false);
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    inputNameRef.current.focus();
+  }, []);
+
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    onSubmit: async (values) => {
+      try {
+        await loginSchema.validate(values);
+        setFormValid(true);
+      } catch {
+        setFormValid(false);
+      }
+    },
+  });
+
+  return (
+    <Container fluid className="h-100">
+      <Row className="justify-content-center align-content-center h-100">
+        <Col xs={12} md={8} xxl={6}>
+          <Card className="shadow-sm">
+            <Card.Body className="row p-5">
+              <Col xs={12} md={6} className="d-flex align-items-center justify-content-center">
+                <img src={loginImg} className="rounded-circle" alt={t('entry')} />
+              </Col>
+              <Form onSubmit={formik.handleSubmit} className="col-12 col-md-6 mt-3 mt-mb-0">
+                <h1 className="text-center mb-4">{t('entry')}</h1>
+
+                <Form.Floating className="mb-3">
+                  <Form.Control
+                    name="username"
+                    autoComplete="username"
+                    required
+                    placeholder={t('placeholders.login')}
+                    id="username"
+                    onChange={formik.handleChange}
+                    value={formik.values.username}
+                    isInvalid={formValid}
+                    ref={inputNameRef}
+                  />
+                  <Form.Label htmlFor="username">{t('placeholders.login')}</Form.Label>
+                </Form.Floating>
+
+                <Form.Floating className="mb-4">
+                  <Form.Control
+                    name="password"
+                    autoComplete="current-password"
+                    required
+                    placeholder={t('placeholders.password')}
+                    type="password"
+                    id="password"
+                    onChange={formik.handleChange}
+                    value={formik.values.password}
+                    isInvalid={formValid}
+                  />
+                  <Form.Label htmlFor="password">{t('placeholders.password')}</Form.Label>
+                  <div className="invalid-tooltip">{t('invalidFeedback')}</div>
+                </Form.Floating>
+
+                <Button
+                  type="submit"
+                  variant="outline-primary"
+                  className="w-100 mb-3"
+                >
+                  {t('entry')}
+                </Button>
+
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default LoginPage;
