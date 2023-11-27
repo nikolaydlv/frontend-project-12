4278283597f4da876848d23a71e80e6f7ae0dff7
Nikolay <wokewoke@mail.ru>
2023-11-27 19:17:15 +0300

    add slices index.js

---
+++ frontend/src/slices/index.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+import channelsReducer from './channelsSlice.js';
+import messagesReducer from './messagesSlice.js';
+
+export default configureStore({
+  reducer: {
+    channels: channelsReducer,
+    messages: messagesReducer,
+  },
+});
