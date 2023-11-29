c55fd763f94ad285efbd2948378e6be4e74c7cb3
Nikolay <wokewoke@mail.ru>
2023-11-29 11:01:17 +0300

    add store

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import i18next from 'i18next';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
+import { Provider } from 'react-redux';
 
 import App from './components/App.jsx';
 import resources from './locales/index.js';
+import store from './slices/index.js';
 
 const init = async () => {
   const i18n = i18next.createInstance();
@@ -18,7 +20,9 @@ const init = async () => {
 
   return root.render(
     <I18nextProvider i18n={i18n}>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </I18nextProvider>,
   );
 };
