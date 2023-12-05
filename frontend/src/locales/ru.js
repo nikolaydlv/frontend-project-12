aa95b5f1df1f0af51511bdbedb90090df2ec9dbd
Nikolay <wokewoke@mail.ru>
2023-12-05 07:52:34 +0300

    update ru locale

--- frontend/src/locales/ru.js
+++ frontend/src/locales/ru.js
@@ -1,14 +1,10 @@
 export default {
   translation: {
     name: 'Hexlet Chat',
-    placeholders: {
-      login: 'Ваш ник',
-      password: 'Пароль',
-      newMessage: 'Введите сообщение...',
-    },
     entry: 'Войти',
     exit: 'Выйти',
     registration: 'Регистрация',
+    makeRegistration: 'Зарегистрироваться',
     noAccount: 'Нет аккаунта?',
     invalidFeedback: 'Неверные имя пользователя или пароль',
     pageNotFound: 'Страница не найдена',
@@ -19,12 +15,28 @@ export default {
     messageBody: 'Сообщение не может быть пустым',
     send: 'Отправить',
     cancel: 'Отменить',
+
+    registrationRules: {
+      name: 'От 3 до 20 символов',
+      password: 'Не менее 6 символов',
+      passwordEquality: 'Пароли должны совпадать',
+    },
+
+    placeholders: {
+      login: 'Ваш ник',
+      username: 'Имя пользователя',
+      password: 'Пароль',
+      passwordConfirmation: 'Подтвердите пароль',
+      newMessage: 'Введите сообщение...',
+    },
+
     messagesCounter: {
       messages_zero: 'Нет сообщений',
       messages_one: '{{count}} сообщение',
       messages_few: '{{count}} сообщения',
       messages_many: '{{count}} сообщений',
     },
+
     modal: {
       add: 'Добавить канал',
       toggle: 'Переключатель режимов',
@@ -35,6 +47,12 @@ export default {
       confirm: 'Уверены?',
       unique: 'Должно быть уникальным',
       lengthParams: 'От 3 до 20 символов',
+    },
+
+    errors: {
+      invalidFeedback: 'Неверные имя пользователя или пароль',
+      userExist: 'Такой пользователь уже существует',
+      required: 'Обязательное поле',
     },
   },
 };
