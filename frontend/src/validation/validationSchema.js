0b102cc78a622bc1ecc2b57bdff7aaee187a015c
Nikolay <wokewoke@mail.ru>
2023-12-06 11:13:39 +0300

    add registration schema

--- frontend/src/validation/validationSchema.js
+++ frontend/src/validation/validationSchema.js
@@ -1,12 +1,42 @@
 import * as yup from 'yup';
 
-const loginSchema = yup.object().shape({
-  username: yup.string().trim().required(),
-  password: yup.string().required(),
+const registrationSchema = (nameMsg, passwordMsg, equalMsg, requiredMsg) => yup.object().shape({
+  username: yup
+    .string()
+    .trim()
+    .min(3, nameMsg)
+    .max(20, nameMsg)
+    .required(requiredMsg),
+
+  password: yup
+    .string()
+    .trim()
+    .min(6, passwordMsg)
+    .required(requiredMsg),
+
+  passwordConfirmation: yup
+    .string()
+    .trim()
+    .oneOf([yup.ref('password')], equalMsg)
+    .required(requiredMsg),
+});
+
+const loginSchema = (message) => yup.object().shape({
+  username: yup
+    .string()
+    .trim()
+    .required(message),
+
+  password: yup
+    .string()
+    .required(message),
 });
 
 const chatSchema = (message) => yup.object().shape({
-  body: yup.string().trim().required(message),
+  body: yup
+    .string()
+    .trim()
+    .required(message),
 });
 
 const newChannelSchema = (channels, doubleMsg, lengthMsg) => yup.object().shape({
@@ -19,4 +49,9 @@ const newChannelSchema = (channels, doubleMsg, lengthMsg) => yup.object().shape(
     .required(lengthMsg),
 });
 
-export { loginSchema, chatSchema, newChannelSchema };
+export {
+  registrationSchema,
+  loginSchema,
+  chatSchema,
+  newChannelSchema,
+};
