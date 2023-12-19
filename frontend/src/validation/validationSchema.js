2b0aa387ffc80a3b6c21399f68227144fac10f25
Nikolay <wokewoke@mail.ru>
2023-12-19 12:32:21 +0300

    refactoring validation schema

--- frontend/src/validation/validationSchema.js
+++ frontend/src/validation/validationSchema.js
@@ -7,14 +7,12 @@ const registrationSchema = (nameMsg, passwordMsg, equalMsg, requiredMsg) => yup.
     .min(3, nameMsg)
     .max(20, nameMsg)
     .required(requiredMsg),
-
   password: yup
     .string()
     .trim()
     .min(6, passwordMsg)
     .required(requiredMsg),
-
-  passwordConfirmation: yup
+  confirmPassword: yup
     .string()
     .trim()
     .oneOf([yup.ref('password')], equalMsg)
@@ -26,27 +24,26 @@ const loginSchema = (message) => yup.object().shape({
     .string()
     .trim()
     .required(message),
-
   password: yup
     .string()
     .required(message),
 });
 
 const chatSchema = (message) => yup.object().shape({
-  body: yup
+  messageText: yup
     .string()
     .trim()
     .required(message),
 });
 
 const newChannelSchema = (channels, doubleMsg, lengthMsg) => yup.object().shape({
-  channelName: yup
+  body: yup
     .string()
     .trim()
-    .notOneOf(channels.map((channel) => channel.name), doubleMsg)
+    .required(lengthMsg)
     .min(3, lengthMsg)
     .max(20, lengthMsg)
-    .required(lengthMsg),
+    .notOneOf(channels, doubleMsg),
 });
 
 export {
