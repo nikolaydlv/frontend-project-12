c0af1c12d0cf2edba3ab4f6fe24625196a21b1b0
Nikolay <wokewoke@mail.ru>
2023-12-19 12:30:55 +0300

    move not found page in errors

---
+++ frontend/src/components/Errors/NotFoundPage.jsx
@@ -0,0 +1,25 @@
+import { Image } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+
+import { appPaths } from '../../routes';
+
+import error from '../../assets/404.svg';
+
+const NotFoundPage = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div className="text-center">
+      <Image width="30%" height="30%" alt="Страница не найдена" src={error} fluid />
+      <h1 className="h4 text-muted">{t('pageNotFound')}</h1>
+      <p className="text-muted">
+        {t('redirect')}
+        {' '}
+        <Link to={appPaths.chat}>{t('mainPage')}</Link>
+      </p>
+    </div>
+  );
+};
+
+export default NotFoundPage;
